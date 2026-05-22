646d052e6612c2b2bfd210c06f01b6dcb4b65e0c
mancilla67 <matias.mancillacesped@outlook.es>
2026-05-21 20:14:10 -0400

    refactor: reorganize play execution logic and improve pass.play formatting

--- lua/main.lua
+++ lua/main.lua
@@ -3,7 +3,7 @@ local targetRobotId = 1
 local team = 0
 
 local loop = require("play_engine.pases_demo")
-
+local motor = require("utils.table_to_tactic")
 
 grsim.teleport_robot(targetRobotId, team, 1.0, 1.0, 0.0)
 grsim.teleport_robot(robotId, team, -0.5, -0.5, 0.0)
@@ -11,5 +11,7 @@ grsim.teleport_robot(robotId, team, -0.5, -0.5, 0.0)
 grsim.teleport_ball(0.0, 0.0)
 
 function process()
+   motor.cargar_jugada("lua/utils/plays/pass.play")
+   motor.update(team)
    loop.process(team)
 end
